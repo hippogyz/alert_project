d5983265c20cc79f1248863650c5aa7a3ae31ba7
hippogyz <hippogyz@hotmail.com>
2021-01-09 20:09:29 +0800

    test

--- assets/actors/player/player.gd
+++ assets/actors/player/player.gd
@@ -2,6 +2,9 @@ extends KinematicBody2D
 
 signal try_take_alert_signal()
 signal reduce_rest_time_signal()
+signal change_sight_signal(sight_index)
+
+enum Exhibit {DEFAULT = 0, PLAYER = 1}
 
 export var move_speed : float = 500.0
 var _velocity : Vector2 = Vector2.ZERO
@@ -30,6 +33,10 @@ func _get_direction() -> Vector2 :
 func _handle_input() -> void:
 	if Input.is_action_just_pressed("take_alert") && has_rest_time:
 		emit_signal("try_take_alert_signal")
+	if Input.is_action_just_pressed("special_sight"):
+		emit_signal("change_sight_signal", 1)
+	if Input.is_action_just_released("special_sight"):
+		emit_signal("change_sight_signal", 0)
 	#some other input...
 
 func reduce_rest_time() -> void:
