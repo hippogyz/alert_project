7bee1434b7e22a032fdc9e0cd3b1bfe5a32efbc0
Jueast <jueastwang@outlook.com>
2021-01-09 10:47:34 +0800

    init git repo

---
+++ assets/actors/npc/PathFollow2D.gd
@@ -0,0 +1,12 @@
+extends PathFollow2D
+
+export var velocity : float = 300.0
+var direction = 1
+
+func _process(delta: float) -> void:
+	offset += delta * velocity * direction
+
+	if unit_offset == 1:
+		direction = -1
+	if unit_offset == 0:
+		direction = 1
