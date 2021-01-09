57ddb33ef423b5eb2b3e9f86dbc54144186ab5e6
hippogyz <hippogyz@hotmail.com>
2021-01-09 19:52:24 +0800

    change orbit z_index

--- assets/actors/npc/npc.gd
+++ assets/actors/npc/npc.gd
@@ -1,4 +1,4 @@
-extends Path2D
+extends Node2D
 
 enum Exhibit {DEFAULT = 0, PLAYER = 1}
 
@@ -7,7 +7,8 @@ var exhibit_mode = Exhibit.DEFAULT
 export var default_color : Color
 export var player_color : Color
 export var line_width : float = 3.0
-
+export (NodePath) onready var npc_path_nodepath
+onready var npc_path = get_node(npc_path_nodepath)
 
 func _ready() -> void:
 	update()
@@ -20,12 +21,12 @@ func change_exhibit_mode(mode) -> void:
 	update()
 
 func _draw() -> void:
-	if curve == null:
+	if npc_path.curve == null:
 		return
 	match exhibit_mode:
 		Exhibit.DEFAULT:
-			draw_polyline(curve.get_baked_points(), default_color, line_width, true)
+			draw_polyline(npc_path.curve.get_baked_points(), default_color, line_width, true)
 		Exhibit.PLAYER:
-			draw_polyline(curve.get_baked_points(), player_color, line_width , true)
+			draw_polyline(npc_path.curve.get_baked_points(), player_color, line_width , true)
 		_:
-			draw_polyline(curve.get_baked_points(), default_color, line_width , true)
+			draw_polyline(npc_path.curve.get_baked_points(), default_color, line_width , true)
